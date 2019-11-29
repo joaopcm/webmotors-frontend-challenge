bcf04acecdd3f9ebe40c18f206545aeae0d91047
João Melo <jopcmelo@gmail.com>
2019-11-29 14:27:13 -0300

    chore: Menu tab is done

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { MdDirectionsCar, MdMotorcycle } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, Content, Header, HeaderItem, Card } from './styles';
 
 export default function Home() {
-  return <div />;
+  const [size, setSize] = useState('car');
+
+  function handleSetSize(size) {
+    setSize(size);
+  }
+
+  return (
+    <Container>
+      <Content>
+        <Header>
+          <ul>
+            <HeaderItem active={size === 'car'}>
+              <button type="button" onClick={() => handleSetSize('car')}>
+                <div>
+                  <MdDirectionsCar size={32} />
+                </div>
+                <div className="label">
+                  <span>comprar</span>
+                  <h1>Carros</h1>
+                </div>
+              </button>
+            </HeaderItem>
+            <HeaderItem active={size === 'motorcycle'}>
+              <button type="button" onClick={() => handleSetSize('motorcycle')}>
+                <div>
+                  <MdMotorcycle size={32} />
+                </div>
+                <div className="label">
+                  <span>comprar</span>
+                  <h1>Motos</h1>
+                </div>
+              </button>
+            </HeaderItem>
+          </ul>
+
+          <button type="button">Vender meu carro</button>
+        </Header>
+        <Card>Card</Card>
+      </Content>
+    </Container>
+  );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,3 +1,70 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+import colors from '~/styles/colors';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 30px;
+`;
+
+export const Content = styled.div`
+  width: 933px;
+  height: 312px;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  ul {
+    display: flex;
+  }
+`;
+
+export const HeaderItem = styled.li`
+  cursor: pointer;
+  padding: 0 15px 10px;
+  border-bottom: 3px solid
+    ${props => (props.active ? colors.primary : colors.background)};
+
+  button {
+    border: 0;
+    background: transparent;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  div:first-child {
+    padding: 0 10px;
+
+    svg {
+      color: ${props => (props.active ? colors.primary : colors.darkGrey)};
+    }
+  }
+
+  div.label {
+    text-align: left;
+  }
+
+  span {
+    color: ${colors.darkGrey};
+  }
+
+  span {
+    text-transform: uppercase;
+  }
+
+  h1 {
+    color: ${props => (props.active ? colors.primary : colors.darkGrey)};
+    font-weight: lighter;
+    text-transform: uppercase;
+  }
+`;
+
+export const Card = styled.div`
+  background: ${colors.white};
+  padding: 15px 30px;
+`;
